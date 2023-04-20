adc85083aedf9872ba9237fe94a2fcad416be7b1
ChathurangaJayanath <chathuranga.19@cse.mrt.ac.lk>
2023-04-20 21:24:34 +0530

    update invalid file extension message

--- graphql-cli/src/main/java/io/ballerina/graphql/cmd/Constants.java
+++ graphql-cli/src/main/java/io/ballerina/graphql/cmd/Constants.java
@@ -27,10 +27,10 @@ public class Constants {
     public static final String MESSAGE_FOR_MISSING_INPUT_ARGUMENT = "The input file path argument is missing. " +
             "Please provide the path of the GraphQL config file, Ballerina service file or GraphQL schema file " +
             "with -i or --input flag. \ne.g: bal graphql --input <GraphQL configuration file>";
-    public static final String MESSAGE_FOR_INVALID_FILE_EXTENSION = "File \"%s\" is invalid. Supported files are, " +
-            "\nA GraphQL configuration file with .yaml/.yml extension, a Ballerina service file with .bal or a " +
-            "GraphQL schema file with .graphql is required as the input. \nPlease provide the path of the GraphQL " +
-            "config file with -i or --input flag.\ne.g: bal graphql --input <GraphQL configuration file>";
+    public static final String MESSAGE_FOR_INVALID_FILE_EXTENSION = "File \"%s\" is invalid. Supported input files " +
+            "are,\nA GraphQL configuration file with .yaml/.yml extension, \na Ballerina service file with .bal " +
+            "extension or \na GraphQL schema file with .graphql extension. \nPlease provide the path of the input " +
+            " file with -i or --input flag.\ne.g: bal graphql --input <GraphQL configuration file>";
     public static final String MESSAGE_FOR_MISMATCH_MODE_AND_FILE_EXTENSION = "\"%s\" mode is not allowed to used " +
             "with file \"%s\".\nThe mode flag value should be client, service or schema. Input file should be a " +
             "GraphQL configuration file with .yaml/.yml extension, a Ballerina service file with .bal or a " +
