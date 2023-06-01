a3a0907b04c1386ba3a7991ff022f0d6b9ebd33e
ChathurangaJayanath <chathuranga.19@cse.mrt.ac.lk>
2023-06-01 16:16:09 +0530

    add test to check combining updated schema with added parameters in object fields

--- graphql-cli/src/test/java/io/ballerina/graphql/generator/ballerina/ServiceCombinerTest.java
+++ graphql-cli/src/test/java/io/ballerina/graphql/generator/ballerina/ServiceCombinerTest.java
@@ -697,6 +697,52 @@ public class ServiceCombinerTest extends GraphqlTest {
         }
     }
 
+    @Test(description = "Test combining updated schema with added parameters in object fields " +
+            "represented in service class")
+    public void testCombiningUpdatedSchemaWithAddedParametersInObjectFieldsRepresentedInServiceClass()
+            throws Exception {
+        String beforeBalFileName = "typesBeforeAddingParametersInObjectFieldsDefault";
+        String expectedBalFileName = "typesWithAddedParametersInObjectFieldsDefault";
+        String newSchemaFileName = "SchemaWithAddedParametersInObjectFieldsApi";
+        Path updatedBalFilePath = this.resourceDir.resolve(
+                Paths.get("serviceGen", "updatedServices", "onlyLogicImplementation", beforeBalFileName + ".bal"));
+        Path newSchemaPath = this.resourceDir.resolve(
+                Paths.get("serviceGen", "graphqlSchemas", "updated", "removeField", newSchemaFileName + ".graphql"));
+        Path mergedBalFilePath = this.resourceDir.resolve(
+                Paths.get("serviceGen", "expectedServices", "updated", "removeField", expectedBalFileName + ".bal"));
+
+        GraphqlServiceProject newGraphqlProject =
+                new GraphqlServiceProject(ROOT_PROJECT_NAME, newSchemaPath.toString(), "./");
+        Utils.validateGraphqlProject(newGraphqlProject);
+
+        String updatedBalFileContent = String.join(Constants.NEW_LINE, Files.readAllLines(updatedBalFilePath));
+        ModulePartNode updateBalFileNode = NodeParser.parseModulePart(updatedBalFileContent);
+        ServiceTypesGenerator serviceTypesGenerator = new ServiceTypesGenerator();
+        serviceTypesGenerator.setFileName(newSchemaFileName);
+        ModulePartNode nextSchemaNode = serviceTypesGenerator.generateContentNode(newGraphqlProject.getGraphQLSchema());
+
+        ServiceCombiner serviceCombiner = new ServiceCombiner(updateBalFileNode, nextSchemaNode);
+        SyntaxTree mergedSyntaxTree = serviceCombiner.mergeRootNodes();
+        String result = Formatter.format(Formatter.format(mergedSyntaxTree).toString().trim()).trim();
+        String expectedServiceTypesContent = readContentWithFormat(mergedBalFilePath);
+        Assert.assertEquals(expectedServiceTypesContent, result);
+
+        List<String> warningMessages = new ArrayList<>();
+        warningMessages.add("warning: In 'Child' class 'knowsWords' function definition 'word1' parameter added " +
+                "without default value. This can break existing clients.");
+        warningMessages.add(
+                "warning: In 'Child' class 'knowsWords' function definition 'word2' parameter added without default " +
+                        "value. This can break existing clients.");
+        warningMessages.add(
+                "warning: In 'Child' class 'knowsWords' function definition 'word' parameter removed. This can break " +
+                        "existing clients.");
+        List<String> breakingChangeWarnings = serviceCombiner.getBreakingChangeWarnings();
+        Assert.assertTrue(breakingChangeWarnings.size() == 3);
+        for (int i = 0; i < breakingChangeWarnings.size(); i++) {
+            Assert.assertEquals(breakingChangeWarnings.get(i), warningMessages.get(i));
+        }
+    }
+
     @Test(description = "Test combining updated schema with new interface fields")
     public void testNodeParser() throws Exception {
         String balFileName = "typesDocsWithEnumDefault";
