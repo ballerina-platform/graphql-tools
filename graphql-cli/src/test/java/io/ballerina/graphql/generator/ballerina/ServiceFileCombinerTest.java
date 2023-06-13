6cfba793fad27f19a75336cf1f2eabd89ea9b3b4
ChathurangaJayanath <chathuranga.19@cse.mrt.ac.lk>
2023-06-13 10:53:11 +0530

    add test to check generation of service file after adding mutation fields to schema

--- graphql-cli/src/test/java/io/ballerina/graphql/generator/ballerina/ServiceFileCombinerTest.java
+++ graphql-cli/src/test/java/io/ballerina/graphql/generator/ballerina/ServiceFileCombinerTest.java
@@ -59,4 +59,37 @@ public class ServiceFileCombinerTest extends GraphqlTest {
         String expectedServiceContent = readContentWithFormat(mergedBalFilePath);
         Assert.assertEquals(result, expectedServiceContent);
     }
+
+    @Test(description = "Test combining updated schema with added mutation fields")
+    public void testCombiningUpdatedSchemaWithAddedMutationFields()
+            throws ValidationException, IOException, ServiceTypesGenerationException, FormatterException {
+        String newSchemaFileName = "SchemaWithAddedNewMutationFieldsApi";
+        String beforeBalFileName = "serviceBeforeAddingMutationFields";
+        String expectedBalFileName = "serviceWithAddedMutationFields";
+        Path updatedBalFilePath = this.resourceDir.resolve(
+                Paths.get("serviceGen", "updatedServices", beforeBalFileName + ".bal"));
+        Path newSchemaPath = this.resourceDir.resolve(
+                Paths.get("serviceGen", "graphqlSchemas", "updated", newSchemaFileName + ".graphql"));
+        Path mergedBalFilePath = this.resourceDir.resolve(
+                Paths.get("serviceGen", "expectedServices", "updated", expectedBalFileName + ".bal"));
+        GraphqlServiceProject newGraphqlProject =
+                new GraphqlServiceProject(ROOT_PROJECT_NAME, newSchemaPath.toString(), "./");
+        Utils.validateGraphqlProject(newGraphqlProject);
+
+        ServiceTypesGenerator serviceTypesGenerator = new ServiceTypesGenerator();
+        serviceTypesGenerator.setFileName(newSchemaFileName);
+        serviceTypesGenerator.generateSrc(newGraphqlProject.getGraphQLSchema());
+
+        ServiceGenerator serviceGenerator = new ServiceGenerator();
+        serviceGenerator.setFileName(newSchemaFileName);
+        serviceGenerator.setMethodDeclarations(serviceTypesGenerator.getServiceMethodDeclarations());
+
+        String updatedBalFileContent = String.join(Constants.NEW_LINE, Files.readAllLines(updatedBalFilePath));
+        ModulePartNode updateBalFileNode = NodeParser.parseModulePart(updatedBalFileContent);
+        ModulePartNode nextBalFileNode = serviceGenerator.generateContentNode();
+        ServiceFileCombiner serviceFileCombiner = new ServiceFileCombiner(updateBalFileNode, nextBalFileNode);
+        String result = serviceFileCombiner.generateMergedSrc().trim();
+        String expectedServiceContent = readContentWithFormat(mergedBalFilePath);
+        Assert.assertEquals(result, expectedServiceContent);
+    }
 }
---
+++ graphql-cli/src/test/resources/serviceGen/updatedServices/serviceBeforeAddingMutationFields.bal
@@ -0,0 +1,22 @@
+import ballerina/graphql;
+
+configurable int port = 9090;
+
+service SchemaWithAddedNewMutationFieldsApi on new graphql:Listener(port) {
+    resource function get book(int id, string? title) returns Book? {
+        return new Book();
+    }
+
+    resource function get books() returns Book?[]? {
+        return null;
+    }
+
+    resource function get authors() returns Author[] {
+        Author[] authors = [new Author()];
+        return authors;
+    }
+
+    remote function addBook(string title, int authorId) returns Book? {
+        return null;
+    }
+}
